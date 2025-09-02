e611b3dd2f09a7020911288d4c2012974f401ec4
mahaltrupti <truptimahal2004@gmail.com>
2025-09-02 16:21:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+ <script>
+    const box = document.getElementById("myBox");
+
+    box.addEventListener("click", () => {
+      box.style.backgroundColor = 
+        box.style.backgroundColor === "lightgreen" ? "lightblue" : "lightgreen";
+      box.textContent = 
+        box.textContent === "Clicked!" ? "Click Me!" : "Clicked!";
+    });
+  </script>
