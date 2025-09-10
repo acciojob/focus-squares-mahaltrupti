bc453fd5c902b009cc1279e86a6b14fd7933fbd0
mahaltrupti <truptimahal2004@gmail.com>
2025-09-10 12:09:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,25 @@
+<script>
+    const squares = document.querySelectorAll(".focus-sq");
+
+    function resetColors() {
+      squares.forEach(sq => sq.style.background = "#E6E6FA");
+    }
+
+    squares.forEach(square => {
+      square.addEventListener("mouseover", () => {
+        resetColors();
+        squares.forEach(other => {
+          if (other !== square) {
+            other.style.background = "#6F4E37"; // Coffee
+          }
+        });
+      });
+
+      square.addEventListener("mouseout", resetColors);
+    });
+  </script>
 
 
-
-            const boxes = document.querySelectorAll(".box")
-            console.log(boxes)
-
-
-            // Iterate through boxes
-            for(let box of boxes){
-                // Add mouse over event
-                box.addEventListener("mouseover",(e)=>addBgColor(e))
-                box.addEventListener("mouseout",(e)=>addDefaultBgColor())
-
-            }
-
-
-            // Add bg color to other boxes
-            function addBgColor(e){
-                const selectedBoxId = e.target.id
-                for(let i =0;i<boxes.length;i++){
-                    if(boxes[i].id !== selectedBoxId){
-                        boxes[i].style.backgroundColor = "red"
-                    }
-                }
-            }
-
-            // Remove bg color / Add default color to all boxes
-            function addDefaultBgColor(){
-                for(let box of boxes){
-                    box.style.backgroundColor = "rgb(223, 134, 134)"
-                }
-            }
-       
+            
        
        
